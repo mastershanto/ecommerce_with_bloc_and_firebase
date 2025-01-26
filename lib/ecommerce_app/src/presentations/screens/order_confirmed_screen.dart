235e9d1010966a51c660cd9b76ee848dcf5fcf0b
masterShanto <mastershanto@gmail.com>
2025-01-26 12:14:06 +0600

    stateless class created

---
+++ lib/ecommerce_app/src/presentations/screens/order_confirmed_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrderConfirmedScreen extends StatelessWidget {
+  const OrderConfirmedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
