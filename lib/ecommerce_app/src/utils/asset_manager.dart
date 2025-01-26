8aff7962f68a6eae6438ce6adcf02e8d703d5be3
masterShanto <mastershanto@gmail.com>
2025-01-26 10:03:33 +0600

    updated

--- lib/ecommerce_app/src/utils/asset_manager.dart
+++ lib/ecommerce_app/src/utils/asset_manager.dart
@@ -1,3 +1,10 @@
 class Assets{
   static const String APP_LOGO="assets/images/app_logo.svg";
+  // static const String FACEBOOK_IMG="assets/images/app_logo.svg";
+  // static const String TWEETER_IMG="assets/images/app_logo.svg";
+  // static const String GOOGLE_IMG="assets/images/app_logo.svg";
+  static const String FACEBOOK_IMG="assets/images/facebook.png";
+  static const String TWEETER_IMG="assets/images/tweeter_img.png";
+  static const String GOOGLE_IMG="assets/images/google_plus_img.png";
+  static const String CLOUD_LOCK="assets/images/cloud_lock.png";
 }
