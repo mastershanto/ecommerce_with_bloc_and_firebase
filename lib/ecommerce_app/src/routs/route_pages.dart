4919de43cd4a23b1b311e62f4409296d2c29fad5
masterShanto <mastershanto@gmail.com>
2025-01-26 12:38:09 +0600

    new_password, payment, reviews, cart related screens added to routes

--- lib/ecommerce_app/src/routs/route_pages.dart
+++ lib/ecommerce_app/src/routs/route_pages.dart
@@ -1,30 +1,23 @@
-import 'package:ecommerce_with_bloc_and_firebase/ecommerce_app/src/presentations/screens/home_screen.dart';
-import 'package:ecommerce_with_bloc_and_firebase/ecommerce_app/src/presentations/screens/login_screen.dart';
-import 'package:ecommerce_with_bloc_and_firebase/ecommerce_app/src/presentations/screens/signup_screen.dart';
-import 'package:ecommerce_with_bloc_and_firebase/ecommerce_app/src/presentations/screens/welcome_screen.dart';
+import 'package:ecommerce_with_bloc_and_firebase/ecommerce_app/src/presentations/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
-import '../presentations/screens/splash_screen.dart';
 
 part 'routes.dart';
 
 class RoutePages {
   static final ROUTER = GoRouter(
     routes: [
+
+      //forgot_password
       GoRoute(
-        path: Routes.SPLASH_ROUTE,
-        name: Routes.SPLASH_ROUTE,
-        pageBuilder: (context, state) => const MaterialPage(
-          child: SplashScreen(),
+        path: Routes.FORGOT_PASSWORD_ROUTE,
+        name: Routes.FORGOT_PASSWORD_ROUTE,
+        pageBuilder: (context, state)=>MaterialPage(
+          child: ForgotPasswordScreen(),
         ),
       ),
-      GoRoute(
-        path: Routes.HOME_ROUTE,
-        name: Routes.HOME_ROUTE,
-        pageBuilder: (context, state) =>
-            const MaterialPage(child: HomeScreen()),
-      ),
+
+      //login
       GoRoute(
         path: Routes.LOGIN_ROUTE,
         name: Routes.LOGIN_ROUTE,
@@ -33,6 +26,15 @@ class RoutePages {
         ),
       ),
 
+      //new_password
+      GoRoute(
+        path: Routes.NEW_PASSWORD_ROUTE,
+        name: Routes.NEW_PASSWORD_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: NewPasswordScreen()),
+      ),
+
+      //resister
       GoRoute(
         path: Routes.REGISTER_ROUTE,
         name: Routes.REGISTER_ROUTE,
@@ -40,12 +42,83 @@ class RoutePages {
           child: SignupScreen(),
         ),
       ),
+
+      //splash
+      GoRoute(
+        path: Routes.SPLASH_ROUTE,
+        name: Routes.SPLASH_ROUTE,
+        pageBuilder: (context, state) => const MaterialPage(
+          child: SplashScreen(),
+        ),
+      ),
+
+      //welcome
       GoRoute(
         path: Routes.WELCOME_ROUTE,
         name: Routes.WELCOME_ROUTE,
         pageBuilder: (context, state)=>MaterialPage(
           child: WelcomeScreen(),
         ),
+      ),
+
+//_________________________________________________________________________//
+
+      //home
+      GoRoute(
+        path: Routes.ADD_TO_CART_ROUTE,
+        name: Routes.ADD_TO_CART_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: AddCartScreen()),
+      ),
+      //home
+      GoRoute(
+        path: Routes.ADD_NEW_PAYMENT_CARD_ROUTE,
+        name: Routes.ADD_NEW_PAYMENT_CARD_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: AddNewPaymentCardScreen()),
+      ),
+      //home
+      GoRoute(
+        path: Routes.ADD_REVIEW_ROUTE,
+        name: Routes.ADD_REVIEW_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: AddReviewScreen()),
+      ),
+      GoRoute(
+        path: Routes.ADDRESS_ROUTE,
+        name: Routes.ADDRESS_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: AddressScreen()),
+      ),
+      GoRoute(
+        path: Routes.CART_ROUTE,
+        name: Routes.CART_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: CartScreen()),
+      ),
+      GoRoute(
+        path: Routes.HOME_ROUTE,
+        name: Routes.HOME_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: HomeScreen()),
+      ),
+      GoRoute(
+        path: Routes.ORDER_CONFIRMED_ROUTE,
+        name: Routes.ORDER_CONFIRMED_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: OrderConfirmedScreen()),
+      ),
+      GoRoute(
+        path: Routes.PAYMENT_ROUTE,
+        name: Routes.PAYMENT_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: PaymentScreen()),
+      ),
+      GoRoute(
+        path: Routes.REVIEWS_ROUTE,
+        name: Routes.REVIEWS_ROUTE,
+        pageBuilder: (context, state) =>
+        const MaterialPage(child: ReviewsScreen()),
       ),
 
     ],
