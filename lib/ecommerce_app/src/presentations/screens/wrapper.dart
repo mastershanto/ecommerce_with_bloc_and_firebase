1c5c5f9f1c32cd582fabea697e453827c0944c3d
masterShanto <mastershanto@gmail.com>
2025-02-05 19:44:35 +0600

    child added in scaffold

--- lib/ecommerce_app/src/presentations/screens/wrapper.dart
+++ lib/ecommerce_app/src/presentations/screens/wrapper.dart
@@ -8,6 +8,8 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: child,
+    );
   }
 }
