04b8df9e2531a86d028a194e4ef1726b2105825e
masterShanto <mastershanto@gmail.com>
2025-02-05 19:42:49 +0600

    updated

--- lib/ecommerce_app/src/blocs/blocs.dart
+++ lib/ecommerce_app/src/blocs/blocs.dart
@@ -1,5 +1,5 @@
 export 'splash/splash_cubit.dart';
 export 'remember_switch/remember_switch_cubit.dart';
-export 'signup/signup_bloc.dart';
+export 'signup/sign_up_bloc.dart';
 export 'bloc_ecommerce_observer.dart';
 export 'login/login_bloc.dart';
