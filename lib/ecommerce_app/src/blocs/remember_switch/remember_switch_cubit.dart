2ca4793233f468041dbfefa05e696ce558c7302b
masterShanto <mastershanto@gmail.com>
2025-02-05 19:44:13 +0600

    updated

--- lib/ecommerce_app/src/blocs/remember_switch/remember_switch_cubit.dart
+++ lib/ecommerce_app/src/blocs/remember_switch/remember_switch_cubit.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
@@ -8,10 +6,10 @@ part 'remember_switch_state.dart';
 
 class RememberSwitchCubit extends Cubit<RememberSwitchState> {
   RememberSwitchCubit() : super(RememberSwitchInitial());
+  static bool isRemember = true;
 
-  void SwitchToggle(value){
-emit(SwitchChanged(value));
+  void switchToggle(bool value){
+    isRemember = value;
+    emit(SwitchChanged(isRemember));
   }
 }
-  
-
--- lib/ecommerce_app/src/blocs/remember_switch/remember_switch_state.dart
+++ lib/ecommerce_app/src/blocs/remember_switch/remember_switch_state.dart
@@ -1,10 +1,19 @@
+
 part of 'remember_switch_cubit.dart';
 
 @immutable
-sealed class RememberSwitchState{}
+abstract class RememberSwitchState extends Equatable{
+  @override
+  List<Object?> get props => [];
+}
 
-final class RememberSwitchInitial extends RememberSwitchState {}
-final class SwitchChanged extends RememberSwitchState {
+class RememberSwitchInitial extends RememberSwitchState {}
+
+class SwitchChanged extends RememberSwitchState {
   final bool value;
   SwitchChanged(this.value);
+
+  @override
+  List<Object?> get props => [value];
+
 }
